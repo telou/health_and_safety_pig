9b674c98a099cf95f95ec941211b888d8357246d
Terri Hounsom <hounsom.terri@gmail.com>
2019-09-23 20:38:07 +0100

    edited homepage in react

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -2,9 +2,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default () => (
-  <div>
-  <p>
-  Hello
-  </p>
+  <div class="page-background">
+    <div class="container">
+
+      <div class="title-header">
+        <div id="website-name">
+          <h1 class="title">Health and Safety</h1><h1 id="pig">pigs</h1>
+        </div>
+        <div>
+          <p class="body-text">Keep your pigs safe with our pig-latin health and safety keyword translator. </p>
+        </div>
+      </div>
+
+      <div class="phrase-input">
+      </div>
+
+      <div class="phrase-output">
+      </div>
+    </div>
   </div>
   );
