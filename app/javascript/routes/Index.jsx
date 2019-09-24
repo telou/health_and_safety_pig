e1068570b9270e2ff97bbeaaeb9326a9e56e06c4
Terri Hounsom <hounsom.terri@gmail.com>
2019-09-24 10:57:36 +0100

    got the input field working and submitting to the database

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -4,15 +4,11 @@ import Home from "../components/Home";
 import Terms from "../components/Terms";
 import NewTerm from "../components/NewTerm";
 
-
-
-
 export default (
   <Router>
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/terms" exact component={Terms} />
-      <Route path="/term" exact component={NewTerm} />
     </Switch>
   </Router>
 );
