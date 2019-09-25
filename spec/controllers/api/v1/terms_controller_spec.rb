42c7f911dedb4700c6ccc5ffa0c2cde973b6972b
Terri Hounsom <hounsom.terri@gmail.com>
2019-09-25 14:29:13 +0100

    finished testing

--- spec/controllers/api/v1/terms_controller_spec.rb
+++ spec/controllers/api/v1/terms_controller_spec.rb
@@ -1,38 +1,40 @@
 require 'rails_helper'
 
 describe Api::V1::TermsController do
-
-
   describe "GET #index" do
-    it "returns all terms in the database"
-
-
+    it "returns all terms in the database" do
+      get :index
+      expect(response.body).to eq Term.all.to_json
+    end
   end
 
   describe "POST #create" do
     context "with valid attributes" do
+      params = { phrase: "Risk Assessment" }
       it "saves the term in the database" do
-        expect{
-          Term.create(phrase: "Risk Assessment")
-        }.to change(Term,:count).by(1)
+        expect {
+          post(:create, params: { term: params })
+        }.to change(Term, :count).by(1)
       end
-
     end
   end
 
   describe "POST #create" do
     context "with invalid attributes" do
       it "does not save the term in the database" do
-        expect{
+        expect {
           Term.create(phrase: "H")
-        }.not_to change(Term,:count)
+        }.not_to change(Term, :count)
       end
     end
-      # it "renders a json with errors"
   end
 
-  # describe "DELETE #delete" do
-  #   it "deletes the term from the database"
-  #   it "returns a json"
-  # end
+  describe "DELETE #destroy" do
+    term = Term.create(phrase: "Test test")
+    it "delets a term from the database when user clicks delete" do
+      expect {
+        delete :destroy, params: { id: term.id }
+      }.to change(Term, :count).by(-1)
+    end
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -94,3 +94,16 @@ RSpec.configure do |config|
   Kernel.srand config.seed
 =end
 end
+
+require 'capybara/poltergeist'
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, {
+    js_errors: false,
+    phantomjs_options: ['--ignore-ssl-errors=yes', '--ssl-protocol=any'],
+    debug: false,
+    timeout: 500,
+    phantomjs: File.absolute_path(Phantomjs.path)
+  })
+end
+Capybara.javascript_driver = :poltergeist
+Capybara.server_port = 3001
